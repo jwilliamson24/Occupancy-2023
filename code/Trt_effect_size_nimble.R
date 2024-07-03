96b71db2044e851946d521e25327e89ad7579718
jwilliamson24 <jasmine.m.williamson@gmail.com>
2024-07-03 12:52:04 -0700

    Update Trt_effect_size_nimble.R

--- code/Trt_effect_size_nimble.R
+++ code/Trt_effect_size_nimble.R
@@ -19,7 +19,11 @@ library(tidyverse)
 library(mcmcplots)
 library(MCMCvis)
 library(boot)
-source('attach.nimble_v2.R')
+
+#attach package created by Josh Stewart from bayes class -
+#not entirely sure what it does
+source('attach.nimble_v2.R') 
+
 
 load("./all.spp_model.RData")
 attach.nimble(mcmc.output.1$samples)
@@ -28,9 +32,9 @@ attach.nimble(mcmc.output.1$samples)
 trt.int.inv <- inv.logit(TreatmentIntercept) # Inv logit TreatmentIntercept to get Occupancy Estimates
 treatment_matrix <- trt.int.inv
 
- box.colors.1 <- c('lightgreen','steelblue', 'coral2', '#f9d62e','#b967ff' )
+box.colors.1 <- c('lightgreen','steelblue', 'coral2', '#f9d62e','#b967ff' )
 
-# Build matrix of trt - control effects
+# Build matrix of trt minus control effects
 diff.BS <- TreatmentIntercept[,1]-TreatmentIntercept[,5]
 diff.BU <- TreatmentIntercept[,2]-TreatmentIntercept[,5]
 diff.HB <- TreatmentIntercept[,3]-TreatmentIntercept[,5]
